546647e699c8e5d7f2a8c916f268d61eb798cc1e
Jan Krueger <jankrueger1999@gmail.com>
2022-04-16 13:52:24 +0200

    feat(ui/input): add input component

---
+++ smart-chef-ui/src/components/input/Input.tsx
@@ -0,0 +1,23 @@
+import { InputHTMLAttributes } from "react";
+import "./input.css";
+import ErrorIcon from "./error.svg";
+
+function SCInput(props: SCInputProps) {
+  const { error } = props;
+  return (
+    <div className="sc-input">
+      <input
+        {...props}
+        className={props.error ? "sc-input-error sc-input" : "sc-input"}
+      ></input>
+      {error && <img src={ErrorIcon} alt="" className="sc-input-error-icon" />}
+      {error && <div className="sc-input-error">{error}</div>}
+    </div>
+  );
+}
+
+export default SCInput;
+
+export type SCInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  error?: string;
+};
