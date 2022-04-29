575e7db99b291bbfe33d86bc010e5210b4e363c5
Jan Krueger <jankrueger1999@gmail.com>
2022-04-29 18:17:31 +0200

    feat(api/users): add automatic creation of household

--- smart-chef-api/src/smartchef/serializers/user.py
+++ smart-chef-api/src/smartchef/serializers/user.py
@@ -1,16 +1,29 @@
-from ..models.user import User
+from ..models import User
 from .resource import ResourceSerializer
+import smartchef.serializers.household as HouseholdSerializer
 from django.contrib.auth.hashers import make_password
+from django.db.transaction import atomic
 
 
 class UserSerializer(ResourceSerializer):
 
     # Override create method to set the user's password
+    @atomic
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
-        return super(UserSerializer, self).create(validated_data)
+        createdUser: User = super(UserSerializer, self).create(validated_data)
+        # Create household for user
+        householdSerializer = HouseholdSerializer.HouseholdSerializer(data={
+            'name': f"{createdUser.firstName}'s Haushalt",
+            'owner': createdUser.id,
+            "users": [createdUser.id]
+        })
+        householdSerializer.is_valid(raise_exception=True)
+        householdSerializer.save()
+        return createdUser
 
     # Override update method to set the user's password
+
     def update(self, instance, validated_data):
         if 'password' in validated_data:
             validated_data['password'] = make_password(
