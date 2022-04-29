07493040744c3fe315f42c5d0bbf43f535883300
Jan Krueger <jankrueger1999@gmail.com>
2022-04-29 19:34:02 +0200

    feat(api/users): add user to his own household on creation

--- smart-chef-api/src/smartchef/serializers/household.py
+++ smart-chef-api/src/smartchef/serializers/household.py
@@ -1,19 +1,32 @@
 from ..models.household import Household
+from .resource import ResourceSerializer
+from ..models import User
 from rest_framework import serializers
-from .user import UserSerializer
 
 
-class HouseholdSerializer(serializers.ModelSerializer):
+class HouseholdSerializer(ResourceSerializer):
+    users = serializers.SlugRelatedField(
+        slug_field='id', many=True, queryset=User.objects.all())
+
+    owner = serializers.SlugRelatedField(
+        slug_field='id', queryset=User.objects.all())
+
+    def create(self, validated_data):
+        # Check if the context is set
+        if 'request' in self.context:
+            validated_data['owner'] = self.context['request'].user
+        return super(HouseholdSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'users' in validated_data:
+            validated_data['users'].append(self.context['request'].user)
+        return super(HouseholdSerializer, self).update(instance, validated_data)
+
     class Meta:
         model = Household
-        fields = ('id', 'createdAt', 'updatedAt', 'name', 'owner')
+        fields = ('id', 'createdAt', 'updatedAt', 'name', 'owner', 'users')
 
         # users field is optional
         extra_kwargs = {
             'users': {'required': False}
         }
-
-
-class HouseholdUserSerializer(serializers.Serializer):
-    userId = serializers.UUIDField(write_only=True)
-    users = UserSerializer(many=True, read_only=True)
--- smart-chef-api/src/smartchef/serializers/user.py
+++ smart-chef-api/src/smartchef/serializers/user.py
@@ -3,23 +3,29 @@ from .resource import ResourceSerializer
 import smartchef.serializers.household as HouseholdSerializer
 from django.contrib.auth.hashers import make_password
 from django.db.transaction import atomic
+from rest_framework import serializers
 
 
 class UserSerializer(ResourceSerializer):
+
+    households = serializers.SlugRelatedField(
+        slug_field='id', many=True, read_only=True)
 
     # Override create method to set the user's password
     @atomic
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
         createdUser: User = super(UserSerializer, self).create(validated_data)
-        # Create household for user
-        householdSerializer = HouseholdSerializer.HouseholdSerializer(data={
+        # Create household for user with him as the owner and add him to the users list
+        household_data = {
             'name': f"{createdUser.firstName}'s Haushalt",
             'owner': createdUser.id,
-            "users": [createdUser.id]
-        })
-        householdSerializer.is_valid(raise_exception=True)
-        householdSerializer.save()
+            'users': [createdUser.id]
+        }
+        household_serializer = HouseholdSerializer.HouseholdSerializer(
+            data=household_data)
+        household_serializer.is_valid(raise_exception=True)
+        household_serializer.save()
         return createdUser
 
     # Override update method to set the user's password
@@ -33,7 +39,7 @@ class UserSerializer(ResourceSerializer):
     class Meta:
         model = User
         fields = ('id', 'firstName', 'lastName',
-                  'createdAt', 'updatedAt', 'password', 'email')
+                  'createdAt', 'updatedAt', 'password', 'email', 'households')
         extra_kwargs = {
             'password': {'write_only': True},
         }
