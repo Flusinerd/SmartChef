922b78f6ede1cdede945652029a7b926c41a9fa1
Jan Krueger <jankrueger1999@gmail.com>
2022-05-01 22:18:33 +0200

    feat(api/user): add duplicate email handling for user create

--- smart-chef-api/src/smartchef/views/user.py
+++ smart-chef-api/src/smartchef/views/user.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
+from django.http import HttpResponse
 from ..models.user import User
 from ..serializers.user import CreateUserSerializer, UserSerializer
+from django.db import IntegrityError
 from rest_framework import mixins, viewsets, permissions
 from rest_framework.decorators import action
 
@@ -19,7 +21,11 @@ class UserViewSet(viewsets.ModelViewSet):
         return [permissions.IsAuthenticated()]
 
     def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+        # Return a 409 response if the email is already in use
+        try:
+            return super().create(request, *args, **kwargs)
+        except IntegrityError:
+            return HttpResponse(status=409, content={'message': 'Email already in use'})
 
     def get_serializer_class(self):
         if self.action == 'create':
