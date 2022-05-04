0b3fec98a71b90259da9af8982a3d530583177b6
Ralf Weber <ralf.89@web.de>
2022-05-04 10:52:51 +0200

    settings erster entwurf

--- smart-chef-ui/src/pages/settings/Settings.page.tsx
+++ smart-chef-ui/src/pages/settings/Settings.page.tsx
@@ -1,7 +1,39 @@
-// import styles from "./Settings.module.css";
+import styles from "./Settings.module.css";
+import React from "react";
+import SCNavbar from "../../components/navbar/Navbar";
+import Input from "../../components/input/Input";
+// import PasswordStrength from "../../components/passwordStrength/PasswordStrength";
+import Button from "../../components/button/button";
 
 function SCSettingsPage() {
-  return <p>Settings</p>;
+  return (
+    <React.Fragment>
+      <SCNavbar />
+      <div className={styles.wrapper}>
+        <div className={styles["user-settings"]}>
+          <h2>Nutzereinstellungen</h2>
+          <div className={styles.inputfield}>
+            <label htmlFor="changeEmail">E-Mail ändern</label>
+            <Input placeholder="E-Mail Adresse" />
+          </div>
+          <div className={styles.inputfield}>
+            <label htmlFor="changePassword">Passwort ändern</label>
+            <Input placeholder="Passwort ändern" />
+            {/* <PasswordStrength  /> */}
+          </div>
+          <div className={styles.inputfield}>
+            <label htmlFor="againPassword"></label>
+            <Input placeholder="Passwort bestätigen" />
+          </div>
+        </div>
+
+        <div className={styles.actions}>
+            <Button className={} >Änderungen speichern</Button>
+            <Button>Benutzerkonto löschen</Button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default SCSettingsPage;
