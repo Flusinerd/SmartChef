6844bbb33045afd4065da412e144d16dbd6694b8
Jan Krueger <jankrueger1999@gmail.com>
2022-05-05 12:20:02 +0200

    refactor(ui): remove unused variables

--- smart-chef-ui/src/components/ingredients/Ingredients.stories.tsx
+++ smart-chef-ui/src/components/ingredients/Ingredients.stories.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import SCIngredients from "./Ingredients";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Story, Meta } from "@storybook/react";
-import SCIngredient from "../ingredient/Ingredient";
 
 export default {
   title: "Smartchef/Ingredients",
   component: SCIngredients,
 } as Meta;
-
 
 const Template: Story = () => (
   <Router>
