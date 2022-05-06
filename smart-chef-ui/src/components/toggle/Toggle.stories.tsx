a9b0bd7b8f17f9fb69ca9732384b65cd3d9a372c
Jan Krueger <jankrueger1999@gmail.com>
2022-05-06 11:58:55 +0200

    feat(ui/toggle): add react hook form support

--- smart-chef-ui/src/components/toggle/Toggle.stories.tsx
+++ smart-chef-ui/src/components/toggle/Toggle.stories.tsx
@@ -1,19 +1,39 @@
 import React from "react";
-import SCToggle from "./Toggle";
+import SCToggle, { SCToggleProps } from "./Toggle";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Story, Meta } from "@storybook/react";
+import { useForm } from "react-hook-form";
 
 export default {
   title: "Smartchef/Toggle",
   component: SCToggle,
 } as Meta;
 
-const Template: Story = () => (
+const Template: Story<SCToggleProps> = (args) => (
   <Router>
-    <SCToggle >
-      Einbuchen Ausbuchen
-    </SCToggle>
+    <SCToggle {...args}></SCToggle>
   </Router>
 );
 
+const withReactHookForm = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { register, watch } = useForm();
+  console.log(watch("checkbox"));
+  return (
+    <Router>
+      <SCToggle
+        activeLabel="Active"
+        inactiveLabel="Inactive"
+        register={register("checkbox")}
+      ></SCToggle>
+    </Router>
+  );
+};
+
+export const WithHookForm = withReactHookForm.bind({});
+
 export const Default = Template.bind({});
+Default.args = {
+  activeLabel: "Aktiv",
+  inactiveLabel: "Inaktiv",
+};
