d84f0061866a78fe4140e9671c284552beb1f0ef
Marc Wegener <marc.wegener99@gmail.com>
2022-05-06 13:24:25 +0200

    Änderungen vorbereitend für CSS

--- smart-chef-ui/src/components/breadcrumb/Breadcrumb.tsx
+++ smart-chef-ui/src/components/breadcrumb/Breadcrumb.tsx
@@ -8,19 +8,19 @@ function SCBreadcrumbItem({ url, name, index, isLast }:
     <React.Fragment>
       <li itemProp="itemListElement" itemScope
         itemType="https://schema.org/ListItem" key={index} className={styles["breadcrumb-element"]}>
-        <a itemProp="item" href={url} className={styles["breadcrumb-element__link"]}>
-          <span itemProp="name" className={styles["breadcrumb-element__name"]}>{name}</span></a>
+        <a itemProp="item" href={url} className={styles["breadcrumb-element-link"]}>
+          <span itemProp="name" className={styles["breadcrumb-element-name"]}>{name}</span></a>
         <meta itemProp="position" content={index.toString()} />
       </li>
 
-      {isLast ? "" : <span className={styles["breadcrumb-element__divider"]}>{'>'}</span>}
+      {isLast ? "" : <span className={styles["breadcrumb-element-divider"]}>{'>'}</span>}
     </React.Fragment>
   )
 }
 function SCBreadcrumb({ breadcrumbItems }: BreadcrumbProps) {
   return (
     <nav className={styles["c-breadcrumb"]}>
-      <ol itemScope itemType="https://schema.org/BreadcrumbList" className={styles["breadcrumb__list"]}>
+      <ol itemScope itemType="https://schema.org/BreadcrumbList" className={styles["breadcrumb-list"]}>
         {breadcrumbItems.map((item, index, array) =>
           <SCBreadcrumbItem {...item} index={index + 1} isLast={array.length - 1 === index} key={index} />)}
       </ol>
