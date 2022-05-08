ab0956fc8c1ea563d039e6607f172913215d1dac
Ralf Weber <ralf.89@web.de>
2022-05-08 17:15:31 +0200

    Accordion erster Entwurf

--- smart-chef-ui/src/components/accordion/Accordion.tsx
+++ smart-chef-ui/src/components/accordion/Accordion.tsx
@@ -1,24 +1,55 @@
-import React, { useState } from "react";
-import "./Accordion.css";
-import DropArrow from "./dropArrow.svg";
+// import React, { useState } from "react";
+// import "./Accordion.css";
+// import DropArrow from "./dropArrow.svg";
 
-const SCAccordion = (props: SCAccordionProps) => {
+// const SCAccordion = (props: SCAccordionProps) => {
+//   const [show, setShow] = useState(false);
+//   const open = () => {
+//     setShow(!show);
+//   };
+
+//   return (
+//     <div className="accordion-header" onClick={open}>
+//       {`${props.title}   `}
+//       {show ? <img  src={DropArrow} alt="DropArrow"/> : <img style={{transform: 'rotate(180deg)'}} src={DropArrow} alt="DropArrow"/>}
+//       <div className="accordion-body">{show && props.children}</div>
+      
+//     </div>
+//   );
+// };
+
+// export default SCAccordion;
+
+
+import React, { useState } from "react";
+import styles from "./Accordion.module.css";
+import Chevron from "./chevron.svg";
+
+const SCAccordion = ({ title, children } : SCAccordionProps) => {
   const [show, setShow] = useState(false);
-  const open = () => {
+
+  const showContent = () => {
     setShow(!show);
   };
 
   return (
-    <div className="accordion-header" onClick={open}>
-      {`${props.title}   `}
-      {show ? <img  src={DropArrow} alt="DropArrow"/> : <img style={{transform: 'rotate(180deg)'}} src={DropArrow} alt="DropArrow"/>}
-      <div className="accordion-body">{show && props.children}</div>
-      
+    <div className={styles.accordionWrapper}>
+      <div
+        className={styles.accordionTitle}
+        onClick={showContent}
+      >
+        <div className={styles.title}>{title}</div>
+        <div className={styles.action}>
+          <img src={Chevron} alt="chevron" className={show ? styles.rotate : undefined} />
+        </div>
+      </div>
+      {show && <div className={styles.accordionContent}>{children}</div>}
     </div>
   );
 };
 
 export default SCAccordion;
+
 
 export interface SCAccordionProps {
   title?: string;
