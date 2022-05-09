04db5e4a9ed3864f43b6ebe75f249aeb7926e6fe
Ralf Weber <ralf.89@web.de>
2022-05-09 11:09:06 +0200

    Noch nicht rausgefunden, wie man PropType Function angeben kann

--- smart-chef-ui/src/components/fab/Fab.tsx
+++ smart-chef-ui/src/components/fab/Fab.tsx
@@ -1,6 +1,7 @@
 import styles from "./Fab.module.css";
 import Plus from "./Plus.svg";
 
+//sollte eigentlich eine vom Type Function sein
 function SCFab({ showModal }: any) {
   return (
     <button className={styles.fab} onClick={showModal}>
