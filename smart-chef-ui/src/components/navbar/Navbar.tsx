8a8e4e9873fe0a6def3c6492ff59be44c823de79
Ralf Weber <ralf.89@web.de>
2022-05-16 16:22:51 +0200

    mq Navbar test

--- smart-chef-ui/src/components/navbar/Navbar.tsx
+++ smart-chef-ui/src/components/navbar/Navbar.tsx
@@ -1,31 +1,31 @@
-import "./Navbar.css";
+import styles from "./Navbar.module.css";
 import Logo from "./logo.svg";
 import { Link } from "react-router-dom";
 
 function SCNavbar(_props: any) {
   return (
-    <nav className="navbar">
-      <div className="logo">
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
         <Link to="/scan">
           <h1>
-            <span className="color-primary">Smart</span>
+            <span className={styles["color-primary"]}>Smart</span>
             Chef
           </h1>
           <img src={Logo} alt="Logo" />
         </Link>
       </div>
 
-      <ul className="links">
-        <li className="link">
+      <ul className={styles.links}>
+        <li className={styles.link}>
           <Link to="/scan">Scannen</Link>
         </li>
-        <li className="link active">
+        <li className={styles["link active"]}>
           <Link to="/shopping-list">Einkaufsliste</Link>
         </li>
-        <li className="link">
+        <li className={styles.link}>
           <Link to="/recipes">Rezepte</Link>
         </li>
-        <li className="link">
+        <li className={styles.link}>
           <Link to="/settings">Einstellungen</Link>
         </li>
       </ul>
